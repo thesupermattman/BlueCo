c51ffcc6c1d0568ee365e27d8d07682924b2aef7
thesupermattman <matthew.aamlid@hotmail.com>
2024-03-07 16:37:12 +0700

    Initial commit

---
+++ src/main/java/com/aamlid/blueco/BluecoApplication.java
@@ -0,0 +1,13 @@
+package com.aamlid.blueco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aamlid/blueco/BluecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aamlid.blueco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
